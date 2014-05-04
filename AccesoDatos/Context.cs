a37ef37cb237a36c88bebcd6c4c8b10e6f4e513b
Ernesto Mangia <ernesto_mangia@hotmail.com>
2014-05-03 22:40:13 -0300

    Version 1

---
+++ AccesoDatos/Context.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+using Modelo;
+
+namespace AccesoDatos
+{
+    public class Context : DbContext
+    {
+        private DbSet<GerenteModelo> Gerentes { get; set; }
+
+        private DbSet<ProyectoCaracterizadoModelo> ProyectosCaracterizados { get; set; }
+    }
+}
