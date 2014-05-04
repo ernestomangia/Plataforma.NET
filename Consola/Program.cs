a96082f5e8e7b31ed659ceb34829bafd9e9ee428
Ernesto Mangia <ernesto_mangia@hotmail.com>
2014-05-03 22:20:41 -0300

    Arquitectura

---
+++ Consola/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Consola
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Menu
+            Console.WriteLine("¡Bienvenido!");
+            Console.WriteLine("----------------------MENU----------------------");
+            Console.WriteLine("A - ABMC Gerente");
+            Console.WriteLine("B - ABMC Proyecto");
+            Console.WriteLine("C - ABMC Factor");
+            Console.WriteLine("Ingrese una opción: ");
+            var opcion = Console.ReadKey().Key.ToString();
+
+            switch (opcion)
+            {
+                case "A":
+                    
+                    break;
+                case "B":
+                    
+                    break;
+                case "C":
+                    
+                    break;
+                default:
+                    Console.WriteLine("Opción incorrecta. Intente nuevamente");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Modelo/ProyectoCaracterizadoModelo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Modelo
+{
+    public class ProyectoCaracterizadoModelo
+    {
+        [Key]
+        public int Codigo { get; set; }
+
+        [StringLength(50)]
+        public string Titulo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public double ValorCaracterizacion { get; set; }
+
+        [StringLength(50)]
+        public string TipoProyecto { get; set; }
+
+        public GerenteModelo Gerente { get; set; }
+    }
+}
