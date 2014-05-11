405319233c23c1150bae55ba3139948aeb928109
Ernesto Mangia <ernesto_mangia@hotmail.com>
2014-05-11 20:50:54 -0300

    Configuracion EF

---
+++ AccesoDatos/Configuracion/ProyectoCaracterizadoConfiguracion.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+using Modelo;
+
+namespace AccesoDatos.Configuracion
+{
+    public class ProyectoCaracterizadoConfiguracion : EntityTypeConfiguration<ProyectoCaracterizadoModelo>
+    {
+        public ProyectoCaracterizadoConfiguracion()
+        {
+            Property(p => p.Titulo).HasMaxLength(50);
+            Property(p => p.Fecha).IsRequired();
+            Property(p => p.TipoProyecto).HasMaxLength(50).IsRequired();
+
+            this.HasRequired(p => p.Gerente);
+        }
+    }
+}
